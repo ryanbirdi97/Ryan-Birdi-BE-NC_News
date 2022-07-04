3c591e54fe1de6826e0db10b13feea1e27e3b272
ryanbirdi97 <ryanbirdi97@icloud.com>
2022-07-04 15:38:29 +0100

    GET /api/articles/:article_id

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require("express");
-const { getTopics } = require("./controllers/controllers");
+const { getTopics, getArticleId } = require("./controllers/controllers");
+const {
+  psqlErrorHandler,
+  unhandledErrors,
+  handleCustomErrors,
+} = require("./errors/error-handler");
 
 const app = express();
 
 app.use(express.json());
 
 app.get("/api/topics", getTopics);
+
+app.get("/api/articles/:article_id", getArticleId);
+
+//Error Handlers
+app.use(psqlErrorHandler);
+app.use(handleCustomErrors);
+app.use(unhandledErrors);
 
 module.exports = app;
