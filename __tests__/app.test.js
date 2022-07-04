04071f43b85561abca6c07b61b159c0d44ecbb6b
ryanbirdi97 <ryanbirdi97@icloud.com>
2022-07-04 12:52:05 +0100

    03 GET/api/topics

---
+++ __tests__/app.test.js
@@ -0,0 +1,33 @@
+const db = require("../db/connection.js");
+const testData = require("../db/data/test-data/index.js");
+const seed = require("../db/seeds/seed.js");
+const app = require("../app.js");
+const request = require("supertest");
+require("jest-sorted");
+
+beforeEach(() => {
+  return seed(testData);
+});
+
+afterAll(() => {
+  return db.end();
+});
+
+describe("GET /api/topics", () => {
+  test("responds with an array of topic objects", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then((response) => {
+        expect(response.body.topics).toBeInstanceOf(Array);
+        response.body.topics.forEach((topic) => {
+          expect(topic).toEqual(
+            expect.objectContaining({
+              slug: expect.any(String),
+              description: expect.any(String),
+            })
+          );
+        });
+      });
+  });
+});
