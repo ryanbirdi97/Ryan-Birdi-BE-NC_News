66e115ff7ad71b99acc05ed5c10b991ee0c26f29
ryanbirdi97 <ryanbirdi97@icloud.com>
2022-07-05 14:52:34 +0100

    comment count added to article table

--- models/models.js
+++ models/models.js
@@ -8,7 +8,10 @@ exports.fetchTopics = () => {
 
 exports.fetchArticleId = (id) => {
   return db
-    .query(`SELECT * FROM articles WHERE article_id = $1;`, [id])
+    .query(
+      ` SELECT articles.*, COUNT(comment_id) AS comment_count FROM articles LEFT OUTER JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id`,
+      [id]
+    )
     .then((result) => {
       if (result.rows.length === 0) {
         return Promise.reject({
